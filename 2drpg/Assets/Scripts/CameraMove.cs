e950aacf5672113664ff3f645bc11222fb4d8560
enovyt <eboegers@mail.usf.edu>
2018-06-21 12:30:35 -0400

    Player/Tilemap update

---
+++ 2drpg/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    public GameObject player;
+
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
--- 2drpg/Assets/Scripts/PlayerMove.cs
+++ 2drpg/Assets/Scripts/PlayerMove.cs
@@ -8,53 +8,38 @@ public class PlayerMove : MonoBehaviour {
     private float moveSpeed;
 
     private Animator animator;
-
+    private Vector2 lastmove;
 
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
-        animator.SetInteger("direction", 1);
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        Move(GetDirection());
+        Move();
 
     }
 
-    Vector3 GetDirection()
+    void Move()
     {
-        Vector3 dir = new Vector3();
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
-            dir = Vector3.left;
-            animator.SetInteger("direction", 4);
-        }
-        else if (Input.GetKey(KeyCode.D))
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * Time.deltaTime * moveSpeed, 0f, 0f));
+            lastmove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        } else if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
-            dir = Vector3.right;
-            animator.SetInteger("direction", 2);
-        }
-        else if (Input.GetKey(KeyCode.W))
-        {
-            dir = Vector3.up;
-            animator.SetInteger("direction", 3);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            dir = Vector3.down;
-            animator.SetInteger("direction", 1);
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * Time.deltaTime * moveSpeed, 0f));
+            lastmove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
         }
 
-        return dir;
-    }
-
-
-    void Move(Vector3 dir)
-    {
-        transform.Translate(dir * Time.deltaTime * moveSpeed, Space.World);
+        animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        animator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        animator.SetFloat("LastMoveX", lastmove.x);
+        animator.SetFloat("LastMoveY", lastmove.y);
+        animator.SetBool("isMoving", Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0);
     }
 
 
